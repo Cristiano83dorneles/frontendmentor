da7dcc7296a19fe252a21d6bd1fb9c9591116162
Cristiano83dorneles <109388189+Cristiano83dorneles@users.noreply.github.com>
2022-09-06 21:26:57 +0100

    atl

--- gerador de senha/script.js
+++ gerador de senha/script.js
@@ -1,5 +1,13 @@
-function pointermove() {
-    let força = document.getElementById('forca')
-    let res = document.getElementById('res')
-    console.log('ok')
+function gerador() {
+    let barraforça = document.getElementById('itamanho')
+    let valorbarra = Number(barraforça.value)
+    
+    let gerador = document.getElementById('generate')
+    let senha = document.getElementById('password')
+
+    senha.innerHTML = `${valorbarra} `
+}
+
+if (valorbarra ==  ) {
+
 }
